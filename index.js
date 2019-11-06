6f44f84078426bc743520a1712c859af9c398e70
Reece Pritchard <rapritchard96@gmail.com>
2019-11-06 21:51:31 +0000

    Adding some changes

--- index.js
+++ index.js
@@ -49,11 +49,11 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>');
 });
 
-app.get('/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
-app.get('/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res) => {
     const id = Number(req.params.id);
     const note = notes.find(note => note.id === id);
     if(note) {
@@ -63,7 +63,7 @@ app.get('/notes/:id', (req, res) => {
     }
 });
 
-app.post('/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     const body = req.body;
 
     if(!body.content) {
@@ -84,7 +84,7 @@ app.post('/notes', (req, res) => {
     res.json(note);
 });
 
-app.delete('/notes/:id', (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
     const id = Number(req.params.id);
     notes = notes.filter(note => note.id !== id);
 
