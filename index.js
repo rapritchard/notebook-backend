67803d3c01cecacc337a714a5eecdda50cf073c5
Reece Pritchard <rapritchard96@gmail.com>
2019-11-08 23:17:24 +0000

    Changed wrong code, fixed it

--- index.js
+++ index.js
@@ -30,13 +30,9 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const note = notes.find(note => note.id === id);
-    if(note) {
-        res.json(note);
-    } else {
-        res.status(404).end();
-    }
+    Note.findById(req.params.id).then(note => {
+        res.json(note.toJSON());
+    });
 });
 
 app.post('/api/notes', (req, res) => {
@@ -61,9 +57,10 @@ app.post('/api/notes', (req, res) => {
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    Note.findById(req.params.id).then(note => {
-        res.json(note.toJSON());
-    });
+    const id = Number(req.params.id);
+    notes = notes.filter(note => note.id !== id);
+
+    res.status(204).end();
 });
 
 const unknownEndpoint = (req, res) => {
