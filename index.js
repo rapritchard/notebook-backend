25041b8675d165d55f83e80d2b3b4bfe6727af69
Reece Pritchard <rapritchard96@gmail.com>
2019-11-08 23:42:33 +0000

    Added error handling and updated delete and put to work with mongoDB

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const cors = require('cors');
 
 const app = express();
 const Note = require('./models/note');
-app.use(cors());
-app.use(bodyParser.json());
 app.use(express.static('build'));
+app.use(bodyParser.json());
+app.use(cors());
+
 
 const requestLogger = (req, res, next) => {
     console.log('Method:', req.method);
@@ -29,10 +30,16 @@ app.get('/api/notes', (req, res) => {
     });
 });
 
-app.get('/api/notes/:id', (req, res) => {
-    Note.findById(req.params.id).then(note => {
-        res.json(note.toJSON());
-    });
+app.get('/api/notes/:id', (req, res, next) => {
+    Note.findById(req.params.id)
+        .then(note => {
+            if(note) {
+                res.json(note.toJSON());
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(error => next(error));
 });
 
 app.post('/api/notes', (req, res) => {
@@ -56,11 +63,27 @@ app.post('/api/notes', (req, res) => {
 
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    notes = notes.filter(note => note.id !== id);
+app.put('/api/notes/:id', (req, res, next) => {
+    const body = req.body;
 
-    res.status(204).end();
+    const note = {
+        content: body.content,
+        important: body.important,
+    };
+
+    Note.findByIdAndUpdate(req.params.id, note, { new: true })
+        .then(updatedNote => {
+            res.json(updatedNote.toJSON());
+        })
+        .catch(error => next(error));
+});
+
+app.delete('/api/notes/:id', (req, res, next) => {
+    Note.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
 const unknownEndpoint = (req, res) => {
@@ -68,8 +91,20 @@ const unknownEndpoint = (req, res) => {
         error: 'unknown endpoint'
     });
 };
-
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+      return res.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+// handler of requests with result to errors
+app.use(errorHandler)
 
 const PORT = `${process.env.PORT}` || 3001;
 app.listen(PORT, () => {
