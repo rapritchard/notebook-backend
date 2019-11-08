358258bc27d78135003769c2f40e621e39350907
Reece Pritchard <rapritchard96@gmail.com>
2019-11-08 23:01:57 +0000

    Added MongoDB database

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+const Note = require('./models/note');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('build'));
@@ -17,40 +19,14 @@ const requestLogger = (req, res, next) => {
 
 app.use(requestLogger);
 
-let notes = [  
-    {    
-        id: 1,   
-        content: "HTML is easy",    
-        date: "2019-05-30T17:30:31.098Z",    
-        important: true  
-    },  
-    {    
-        id: 2,    
-        content: "Browser can execute only Javascript",    
-        date: "2019-05-30T18:39:34.091Z",    
-        important: false  
-    },  
-    {    
-        id: 3,    
-        content: "GET and POST are the most important methods of HTTP protocol",    
-        date: "2019-05-30T19:20:14.298Z",    
-        important: true  
-    }
-]
-
-const generateId = () => {
-    const maxId = notes.length > 0 
-        ? Math.max(...notes.map(n => n.id)) 
-        : 0;
-    return maxId + 1;
-}
-
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>');
 });
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    Note.find({}).then(notes => {
+        res.json(notes.map(note => note.toJSON()));
+    });
 });
 
 app.get('/api/notes/:id', (req, res) => {
@@ -72,23 +48,22 @@ app.post('/api/notes', (req, res) => {
         });
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(),
-    };
+    });
 
-    notes = notes.concat(note);
+    note.save().then(savedNote => {
+        res.json(savedNote.toJSON());
+    });
 
-    res.json(note);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    notes = notes.filter(note => note.id !== id);
-
-    res.status(204).end();
+    Note.findById(req.params.id).then(note => {
+        res.json(note.toJSON());
+    });
 });
 
 const unknownEndpoint = (req, res) => {
@@ -99,7 +74,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = `${process.env.PORT}` || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
