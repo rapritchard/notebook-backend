c2732bc66e37e03f5c34d7516b6a7b557c1a0d6c
Reece Pritchard <rapritchard96@gmail.com>
2019-11-11 21:27:24 +0000

    Added eslint to project

--- mongo.js
+++ mongo.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose');
 
 if(process.argv.length < 3) {
-    console.log('give password as argument');
-    process.exit(1);
+	console.log('give password as argument');
+	process.exit(1);
 }
 
 const password = process.argv[2];
 
-const url = 
+const url =
     `mongodb+srv://fullstack:${password}@cluster0-jpuqw.mongodb.net/note-app?retryWrites=true&w=majority`;
 
 mongoose.connect(url, { useNewUrlParser: true });
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+	content: String,
+	date: Date,
+	important: Boolean,
 });
 
 const Note = mongoose.model('Note', noteSchema);
 
 const note = new Note({
-    content: 'We love coding!',
-    date: new Date(),
-    important: true,
+	content: 'We love coding!',
+	date: new Date(),
+	important: true,
 });
 
 // note.save().then(response => {
@@ -32,9 +32,9 @@ const note = new Note({
 // });
 
 Note.find({ important: true }).then(result => {
-    result.forEach(note => {
-        console.log(note);
-    });
-    mongoose.connection.close();
+	result.forEach(note => {
+		console.log(note);
+	});
+	mongoose.connection.close();
 });
 
