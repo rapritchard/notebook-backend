d963b00aa8265ea33bb9cc2fd0d7b5af16b2669e
Reece Pritchard <rapritchard96@gmail.com>
2019-11-11 20:23:31 +0000

    uibuild

---
+++ build/precache-manifest.7cc8ca8946fabe69e10c54071a9f4e0a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19eb89216c34155cdc224b050879a63e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25dfec952ecfbc466e76",
+    "url": "/static/js/2.9cb266a0.chunk.js"
+  },
+  {
+    "revision": "13b41a1633ad40e1112f",
+    "url": "/static/js/main.0c2226f3.chunk.js"
+  },
+  {
+    "revision": "7a2db659b282bd2c122b",
+    "url": "/static/js/runtime-main.86d5872e.js"
+  }
+]);
